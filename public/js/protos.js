ef9d92f59b0e3851337e5c3fb5f4715153a240bb
GioGiglio <giovannimaria.giglio@gmail.com>
2018-06-21 22:01:23 +0200

    Add: encapsulation to objects

--- public/js/protos.js
+++ public/js/protos.js
@@ -1,30 +1,70 @@
 function Song(id, title, artist, album, imageUrl, path){
-	this.id = id;
-	this.title = title;
-	this.artist = artist;
-	this.album = album;
-	this.imageUrl = imageUrl;
-	this.path = path;
+	
+	Object.defineProperties(this, {
+		'id': {
+			value : id,
+			writable : false
+		},
+		'title': {
+			value : title,
+			writable : false
+		},
+		'artist': {
+			value : artist,
+			writable : false
+		},
+		'album': {
+			value : album,
+			writable : false
+		},
+		'imageUrl': {
+			value : imageUrl,
+			writable : false
+		},
+		'path': {
+			value : path,
+			writable : false
+		}
+	});
 }
 
-
 function Playlist (id, name){
-	this.id = id;
-	this.name = name;
-	this.songs = [];
-	this.addSong = function(song){
-		this.songs.push(song);
-	}
-	this.removeSong = function(id){
-		var index = this.songs.indexOf(this.songs.find((x) => x.id == id));
-		if (index != -1){
-			this.songs.splice(index,1);
+
+	Object.defineProperties(this, {
+		'id': {
+			value : id,
+			writable : false
+		},
+		'name': {
+			value : name,
+			writable : false
+		},
+		'songs': {
+			value : [],
+			writable : true
+		},
+		'addSong': {
+			value : function(song){
+				this.songs.push(song);
+			},
+			writable : false
+		},
+		'removeSong': {
+			value : function(id){
+				var index = this.songs.indexOf(this.songs.find((x) => x.id == id));
+				if (index != -1){
+					this.songs.splice(index,1);
+				}
+				else {
+					throw new Error ('Playlist.removeSong(' + id + '): No such song having such id');
+				}
+			},
+			writable : false
+		},
+		'removeAllSongs': {
+			value : function(){
+				this.songs = [];
+			}
 		}
-		else {
-			throw new Error ('Playlist.removeSong(' + id + '): No such song having such id');
-		}
-	}
-	this.removeAllSongs = function(){
-		this.songs = [];
-	}
+	});
 }
